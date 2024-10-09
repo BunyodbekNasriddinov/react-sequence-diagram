760758790e5671147122d920b7c138f5599b4673
BunyodbekNasriddinov <nasriddinoffbunyodbek@gmail.com>
2024-10-09 21:13:30 +0500

    update: header ui

--- src/SequenceDiagram.jsx
+++ src/SequenceDiagram.jsx
@@ -4,17 +4,10 @@ import App from './views/App';
 
 const SequenceDiagram = () => {
 	return (
-		<div
-			className={this.props.className}
-			ref={(div) => {
-				this.div = div;
-			}}
-		>
-			<App
-				state={store.getState()}
-				dispatch={dispatch}
-			/>
-		</div>
+		<App
+			state={store.getState()}
+			dispatch={dispatch}
+		/>
 	);
 };
 
